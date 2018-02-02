53b8dc38e0c5a8765232d5347ca554d1b5071cfc
関大智 <10bvb25hal45016@gmail.com>
2018-02-02 15:33:28 +0900

    .vueファイルの新規作成テスト

---
+++ src/components/CountUpDown.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+h1 CountUpDown
+  p {{ msg }}
+</template>
+
+<script>
+export default {
+  name: 'CountUpDown',
+  data () {
+    return {
+      msg: 'agoooonagooon'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
