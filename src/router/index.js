836b40930910bdc13ec0605b06ebb30cc92439a2
関大智 <10bvb25hal45016@gmail.com>
2018-02-08 12:33:55 +0900

    ログインしていない場合はログインページへリダイレクト

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import firebase from 'firebase'
 import ChatUserCreatePage from '@/components/ChatUserCreatePage'
 import SiteTopPage from '@/components/SiteTopPage'
 import LoginMenuPage from '@/components/LoginMenuPage'
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
   routes: [
+    {
+      path: '*',
+      redirect: 'login'
+    },
     {
       path: '/',
       name: 'SiteTopPage',
-      component: SiteTopPage
+      component: SiteTopPage,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/ChatUserCreatePage',
@@ -25,3 +33,24 @@ export default new Router({
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  let requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  let currentUser = firebase.auth().currentUser
+  if (requiresAuth) {
+    // ログインされているか認証
+    // もしされていないならば、ログインページにリダイレクトします。
+    if (!currentUser) {
+      next({
+        path: '/login',
+        query: { redirect: to.fullPath }
+      })
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
+})
+
+export default router
